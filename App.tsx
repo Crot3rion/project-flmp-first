3bfe517637926ec1aebe97638f4dd39fcdebd5a7
marius <marius.schmid8@gmail.com>
2022-12-18 14:42:14 +0100

    Create Buttons and Navigation for Register and Login

--- App.tsx
+++ App.tsx
@@ -1,12 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
+import React from "react";
 import { StyleSheet, Text, View } from 'react-native';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Landing from "./components/auth/Landing";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Landing">
+          <Stack.Screen name = "Landing" component={Landing} options={{headerShown: false}}/>
+        </Stack.Navigator>
+      </NavigationContainer>
   );
 }
 
@@ -17,4 +26,5 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
 });
